1c86e3709add51cd54e729bb0f5be35479e3be66
LDL_ZHOU <zhousong@hmbpower.com>
2025-07-21 20:19:28 +0800

    1.CAN 发送接收ID配置

--- JDL _AC_Ultra-Fast_Charger/Source/BSW/MCAL/Mcal_Can/Mcal_Can_Cfg.c
+++ JDL _AC_Ultra-Fast_Charger/Source/BSW/MCAL/Mcal_Can/Mcal_Can_Cfg.c
@@ -39,15 +39,15 @@ Mcal_CanTxChannelCfg_t Mcal_CanTxChannelCfgTable[MCAL_CAN_TX_MAX_NUMBER] =
         [MCAL_CAN1_TX_CH] = {
             .CanTxChannel = MCAL_CAN1_TX_CH,
             .CanHandle = &hcan1,
-            .TxHeader.ExtId = MCAL_CAN1_TX_ID,                            // Extended identifier (29 bits)
+            .TxHeader.ExtId = MCAL_CAN1_TX_TEST_ID,                       // Extended identifier (29 bits)
             .TxHeader.IDE = CAN_ID_EXT,                                   // Extended frame
             .TxHeader.RTR = CAN_RTR_DATA,                                 // Data frame
             .TxHeader.DLC = 8,                                            // Data length
             .TxHeader.StdId = 0,                                          // Standard identifier (11 bits)
             .TxHeader.TransmitGlobalTime = DISABLE,                       // use the global timestamp
         },
-        [MCAL_CAN1_TX_CCP] = {
-          .CanTxChannel = MCAL_CAN1_TX_CCP,
+        [MCAL_CAN_TX_CCP] = {
+          .CanTxChannel = MCAL_CAN_TX_CCP,
           .CanHandle = &hcan2,
           .TxHeader.ExtId = 0,                                             // Extended identifier (29 bits)
           .TxHeader.IDE = CAN_ID_STD,                                      // Standard frame
@@ -55,29 +55,69 @@ Mcal_CanTxChannelCfg_t Mcal_CanTxChannelCfgTable[MCAL_CAN_TX_MAX_NUMBER] =
           .TxHeader.DLC = 8,                                               // Data length
           .TxHeader.StdId = MCAL_CAN1_CCP_TX_ID,                           // Standard identifier (11 bits)
           .TxHeader.TransmitGlobalTime = DISABLE,                          // use the global timestamp
+        },
+        [MCAL_CAN1_TX_SECC_MSG1] = {
+            .CanTxChannel = MCAL_CAN1_TX_SECC_MSG1,
+            .CanHandle = &hcan1,
+            .TxHeader.ExtId = MCAL_CAN_SECC_MSG1_TX_ID,                   // Extended identifier (29 bits)
+            .TxHeader.IDE = CAN_ID_EXT,                                   // Extended frame
+            .TxHeader.RTR = CAN_RTR_DATA,                                 // Data frame
+            .TxHeader.DLC = 8,                                            // Data length
+            .TxHeader.StdId = 0,                                          // Standard identifier (11 bits)
+            .TxHeader.TransmitGlobalTime = DISABLE,                       // use the global timestamp
+        },
+        [MCAL_CAN1_TX_SECC_MSG2] = {
+            .CanTxChannel = MCAL_CAN1_TX_SECC_MSG2,
+            .CanHandle = &hcan1,
+            .TxHeader.ExtId = MCAL_CAN_SECC_MSG2_TX_ID,                   // Extended identifier (29 bits)
+            .TxHeader.IDE = CAN_ID_EXT,                                   // Extended frame
+            .TxHeader.RTR = CAN_RTR_DATA,                                 // Data frame
+            .TxHeader.DLC = 8,                                            // Data length
+            .TxHeader.StdId = 0,                                          // Standard identifier (11 bits)
+            .TxHeader.TransmitGlobalTime = DISABLE,                       // use the global timestamp
+        },
+        [MCAL_CAN2_TX_SECC_MSG1] = {
+            .CanTxChannel = MCAL_CAN2_TX_SECC_MSG1,
+            .CanHandle = &hcan2,
+            .TxHeader.ExtId = MCAL_CAN_SECC_MSG1_TX_ID,                   // Extended identifier (29 bits)
+            .TxHeader.IDE = CAN_ID_EXT,                                   // Extended frame
+            .TxHeader.RTR = CAN_RTR_DATA,                                 // Data frame
+            .TxHeader.DLC = 8,                                            // Data length
+            .TxHeader.StdId = 0,                                          // Standard identifier (11 bits)
+            .TxHeader.TransmitGlobalTime = DISABLE,                       // use the global timestamp
+        },
+        [MCAL_CAN2_TX_SECC_MSG2] = {
+            .CanTxChannel = MCAL_CAN2_TX_SECC_MSG2,
+            .CanHandle = &hcan2,
+            .TxHeader.ExtId = MCAL_CAN_SECC_MSG2_TX_ID,                   // Extended identifier (29 bits)
+            .TxHeader.IDE = CAN_ID_EXT,                                   // Extended frame
+            .TxHeader.RTR = CAN_RTR_DATA,                                 // Data frame
+            .TxHeader.DLC = 8,                                            // Data length
+            .TxHeader.StdId = 0,                                          // Standard identifier (11 bits)
+            .TxHeader.TransmitGlobalTime = DISABLE,                       // use the global timestamp
         }
 };
 
 Mcal_CanFilterCfg_t Mcal_CanFilterCfgTable[MCAL_CAN_RX_MAX_NUMBER] =
     {
         [MCAL_CAN1_RX_TEST] = {
-            .CanChannel = MCAL_CAN1_RX_TEST,                                // CAN channel
-            .CanHandle = &hcan1,                                            // CAN Handle
-            .FilterConfig.FilterBank = 10,                                  // the filter bank number
-            .FilterConfig.FilterMode = CAN_FILTERMODE_IDMASK,               // Filter mode
-            .FilterConfig.FilterScale = CAN_FILTERSCALE_32BIT,              // Filter scale
-            .FilterConfig.FilterIdHigh = MCAL_CAN1_FILTER_ID_HIGH,          // High-level ID
-            .FilterConfig.FilterIdLow = MCAL_CAN1_FILTER_ID_LOW,            // Low-level ID
-            .FilterConfig.FilterMaskIdHigh = MCAL_CAN1_FILTER_MASK_ID_HIGH, // Filter mask high-level ID
-            .FilterConfig.FilterMaskIdLow = MCAL_CAN1_FILTER_MASK_ID_LOW,   // Filter mask low-level ID
-            .FilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0,              // Filter FIFO assignment
-            .FilterConfig.FilterActivation = ENABLE,                        // Filter activation
-            .FilterConfig.SlaveStartFilterBank = 14                         // Slave start filter bank
-        },                                                                  /* CAN test */
-        [MCAL_CAN1_RX_CCP] = {
-            .CanChannel = MCAL_CAN1_RX_CCP,                                     // CAN channel
+            .CanChannel = MCAL_CAN1_RX_TEST,                                     // CAN channel
+            .CanHandle = &hcan1,                                                 // CAN Handle
+            .FilterConfig.FilterBank = 10,                                       // the filter bank number
+            .FilterConfig.FilterMode = CAN_FILTERMODE_IDMASK,                    // Filter mode
+            .FilterConfig.FilterScale = CAN_FILTERSCALE_32BIT,                   // Filter scale
+            .FilterConfig.FilterIdHigh = MCAL_CAN1_TEST_FILTER_ID_HIGH,          // High-level ID
+            .FilterConfig.FilterIdLow = MCAL_CAN1_TEST_FILTER_ID_LOW,            // Low-level ID
+            .FilterConfig.FilterMaskIdHigh = MCAL_CAN1_TEST_FILTER_MASK_ID_HIGH, // Filter mask high-level ID
+            .FilterConfig.FilterMaskIdLow = MCAL_CAN1_TEST_FILTER_MASK_ID_LOW,   // Filter mask low-level ID
+            .FilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0,                   // Filter FIFO assignment
+            .FilterConfig.FilterActivation = ENABLE,                             // Filter activation
+            .FilterConfig.SlaveStartFilterBank = 14                              // Slave start filter bank
+        },/* CAN test */
+        [MCAL_CAN_RX_CCP] = {
+            .CanChannel = MCAL_CAN_RX_CCP,                                      // CAN channel
             .CanHandle = &hcan2,                                                // CAN Handle
-            .FilterConfig.FilterBank = 1,                                       // the filter bank number
+            .FilterConfig.FilterBank = 9,                                       // the filter bank number
             .FilterConfig.FilterMode = CAN_FILTERMODE_IDLIST,                   // Filter mode
             .FilterConfig.FilterScale = CAN_FILTERSCALE_16BIT,                  // Filter scale
             .FilterConfig.FilterIdHigh = MCAL_CAN1_CCP_FILTER_ID_HIGH,          // High-level ID
@@ -87,7 +127,35 @@ Mcal_CanFilterCfg_t Mcal_CanFilterCfgTable[MCAL_CAN_RX_MAX_NUMBER] =
             .FilterConfig.FilterFIFOAssignment = CAN_RX_FIFO1,                  // Filter FIFO assignment
             .FilterConfig.FilterActivation = ENABLE,                            // Filter activation
             .FilterConfig.SlaveStartFilterBank = 14                             // Slave start filter bank
-        } /* CAN CCP cfg */
+        },/* CAN CCP cfg */
+        [MCAL_CAN1_RX_MCU_STATUS3] = {
+            .CanChannel = MCAL_CAN1_RX_MCU_STATUS3,                              // CAN channel
+            .CanHandle = &hcan1,                                                 // CAN Handle
+            .FilterConfig.FilterBank = 0,                                        // the filter bank number
+            .FilterConfig.FilterMode = CAN_FILTERMODE_IDLIST,                    // Filter mode
+            .FilterConfig.FilterScale = CAN_FILTERSCALE_32BIT,                   // Filter scale
+            .FilterConfig.FilterIdHigh = MCAL_CAN_MCU_STATUS3_FILTER_ID_HIGH,    // High-level ID
+            .FilterConfig.FilterIdLow = MCAL_CAN_MCU_STATUS3_FILTER_ID_LOW,      // Low-level ID
+            .FilterConfig.FilterMaskIdHigh = MCAL_CAN_MCU_STATUS3_FILTER_MASK_ID_HIGH, // Filter mask high-level ID
+            .FilterConfig.FilterMaskIdLow = MCAL_CAN_MCU_STATUS3_FILTER_MASK_ID_LOW,   // Filter mask low-level ID
+            .FilterConfig.FilterFIFOAssignment = CAN_RX_FIFO0,                   // Filter FIFO assignment
+            .FilterConfig.FilterActivation = ENABLE,                             // Filter activation
+            .FilterConfig.SlaveStartFilterBank = 14                              // Slave start filter bank
+        },/* CAN1 MCU_STATUS3 */
+        [MCAL_CAN2_RX_MCU_STATUS3] = {
+            .CanChannel = MCAL_CAN2_RX_MCU_STATUS3,                              // CAN channel
+            .CanHandle = &hcan2,                                                 // CAN Handle
+            .FilterConfig.FilterBank = 1,                                        // the filter bank number
+            .FilterConfig.FilterMode = CAN_FILTERMODE_IDLIST,                    // Filter mode
+            .FilterConfig.FilterScale = CAN_FILTERSCALE_32BIT,                   // Filter scale
+            .FilterConfig.FilterIdHigh = MCAL_CAN_MCU_STATUS3_FILTER_ID_HIGH,    // High-level ID
+            .FilterConfig.FilterIdLow = MCAL_CAN_MCU_STATUS3_FILTER_ID_LOW,      // Low-level ID
+            .FilterConfig.FilterMaskIdHigh = MCAL_CAN_MCU_STATUS3_FILTER_MASK_ID_HIGH, // Filter mask high-level ID
+            .FilterConfig.FilterMaskIdLow = MCAL_CAN_MCU_STATUS3_FILTER_MASK_ID_LOW,   // Filter mask low-level ID
+            .FilterConfig.FilterFIFOAssignment = CAN_RX_FIFO1,                   // Filter FIFO assignment
+            .FilterConfig.FilterActivation = ENABLE,                             // Filter activation
+            .FilterConfig.SlaveStartFilterBank = 14                              // Slave start filter bank
+        }/* CAN2 MCU_STATUS3 */
 };
 
 Mcal_CanCtrlCfg_t Mcal_CanCtrl = {0};
@@ -120,7 +188,7 @@ void Mcal_Can_RxCycBufCfg_Init(void)
     /* Receive Buff requests */
     CycBufRet = MCAL_CYCBUF_OPEN_CHAN(&Mcal_CanCtrl.Buf[i].RcvCycBufID,
                                       Mcal_CanCtrl.Buf[i].RcvCycBuf,
-                                      MCAL_CAN1_RX_CYC_LEN);
+                                      MCAL_CAN_RX_CYC_LEN);
     if (CycBufRet != MCAL_CYCBUF_RET_SUCCESS)
     {
       MCAL_ERROR("%s Err!\n\r", __FUNCTION__);
@@ -135,29 +203,24 @@ void Mcal_Can_RxCycBufCfg_Init(void)
  *         the TxMailbox used to store the Tx message.
  *         This parameter can be a value of @arg CAN_Tx_Mailboxes.
  * @param  msg Data address.
- * @param  len Data length.
+ * @param  len Data length.Must equal to 8.
  * @retval @McalRetVal_t
  */
 McalRetVal_t Mcal_Can_Send_Msg(Mcal_CanTxChannel_Enum_t Channel, uint8_t *msg, uint8_t len)
 {
   McalRetVal_t ret = MCAL_RET_SUCCESS; /* ret init */
-  uint8_t i = 0;
 
-  if (len > 8 && Channel > MCAL_CAN1_TX_CCP) /* Check whether the data length exceeds 8 bytes */
+  if (len != 8 && Channel >= MCAL_CAN_TX_MAX_NUMBER) /* Check whether the data length exceeds 8 bytes */
   {
     return MCAL_RET_PARAMETE_ERROR;
   }
-
   if (Mcal_CanTxChannelCfgTable[Channel].CanHandle == NULL)
   {
     MCAL_ERROR("%s Err!\r\n", __func__);
     return MCAL_RET_UNEXPECTED_ERROR;
   }
 
-  for (i = 0; i < len; i++)
-  {
-    Mcal_CanCtrl.sendData[i] = msg[i];
-  }
+  memcpy(Mcal_CanCtrl.sendData, msg, 8); /* copy data */
 
   if (HAL_CAN_IsTxMessagePending(Mcal_CanTxChannelCfgTable[Channel].CanHandle, CAN_TX_MAILBOX0 | CAN_TX_MAILBOX1 | CAN_TX_MAILBOX2) != 0)
   {
@@ -191,7 +254,7 @@ uint32_t Mcal_Can_Get_TxMailbox(Mcal_CanTxChannel_Enum_t Channel)
   }
   else
   {
-    return -1; // Invalid channel
+    return 0xffffffff; // Invalid channel
   }
 }
 
@@ -239,20 +302,38 @@ uint32_t Mcal_Can_Receive_Msg(Mcal_CanRxChannel_Enum_t Channel, uint8_t *data, u
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *CanNum)
 {
   uint8_t chNum = 0;
-
-  MCAL_DEBUG("%s \r\n", __func__);
+  // MCAL_DEBUG("%s \r\n", __func__);
   for (chNum = 0; chNum < MCAL_CAN_RX_MAX_NUMBER; chNum++)
   {
     if (CanNum == Mcal_CanFilterCfgTable[chNum].CanHandle && CAN_RX_FIFO0 == Mcal_CanFilterCfgTable[chNum].FilterConfig.FilterFIFOAssignment)
     {
       HAL_CAN_GetRxMessage(Mcal_CanFilterCfgTable[chNum].CanHandle, Mcal_CanFilterCfgTable[chNum].FilterConfig.FilterFIFOAssignment, &Mcal_CanCtrl.RxHeader, Mcal_CanCtrl.rcvData);
-      if (Mcal_CanCtrl.RxHeader.DLC > 0 && Mcal_CanCtrl.RxHeader.ExtId == MCAL_CAN1_RX_ID)
+      /* Check if the received message is from CAN1_RX_TEST */
+      if (Mcal_CanCtrl.RxHeader.DLC > 0 && Mcal_CanCtrl.RxHeader.ExtId == MCAL_CAN1_RX_TEST_ID)
       {
-        MCAL_CYCBUF_WRITE(Mcal_CanCtrl.Buf[MCAL_CAN1_RX_TEST].RcvCycBufID, Mcal_CanCtrl.rcvData, Mcal_CanCtrl.RxHeader.DLC);
-        if (0 != Mcal_CanCtrl.RxHeader.DLC % 8)
+        if (8 == Mcal_CanCtrl.RxHeader.DLC)
         {
-          MCAL_CYCBUF_WRITE(Mcal_CanCtrl.Buf[MCAL_CAN1_RX_TEST].RcvCycBufID, 0, 8 - (Mcal_CanCtrl.RxHeader.DLC % 8)); /* Fill the remaining bytes with 0 */
+          MCAL_CYCBUF_WRITE(Mcal_CanCtrl.Buf[MCAL_CAN1_RX_TEST].RcvCycBufID, Mcal_CanCtrl.rcvData, Mcal_CanCtrl.RxHeader.DLC);
+        }else
+        {
+          MCAL_ERROR("%s ID:%x RcvErr!\n\r", __FUNCTION__, MCAL_CAN1_RX_TEST_ID);
         }
+      }
+      /* Check if the received message is from CNA1_MCU_STATUS3 */
+      else if (Mcal_CanCtrl.RxHeader.DLC > 0 && Mcal_CanCtrl.RxHeader.ExtId == MCAL_RX_MCU_STATUS3_ID)
+      {
+        if (8 == Mcal_CanCtrl.RxHeader.DLC)
+        {
+          MCAL_CYCBUF_WRITE(Mcal_CanCtrl.Buf[MCAL_CAN1_RX_MCU_STATUS3].RcvCycBufID, Mcal_CanCtrl.rcvData, Mcal_CanCtrl.RxHeader.DLC);
+        }
+        else
+        {
+          MCAL_ERROR("%s ID:%x RcvErr!\n\r", __FUNCTION__, MCAL_RX_MCU_STATUS3_ID);
+        }
+      }
+      else
+      {
+        // Handle other messages if necessary
       }
     }
   }
@@ -260,20 +341,36 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *CanNum)
 
 void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *CanNum)
 {
-    uint8_t chNum = 0;
-
-    MCAL_DEBUG("%s \r\n", __func__);
-    for (chNum = 0; chNum < MCAL_CAN_RX_MAX_NUMBER; chNum++)
+  uint8_t chNum = 0;
+  // MCAL_DEBUG("%s \r\n", __func__);
+  for (chNum = 0; chNum < MCAL_CAN_RX_MAX_NUMBER; chNum++)
+  {
+    if (CanNum == Mcal_CanFilterCfgTable[chNum].CanHandle && CAN_RX_FIFO1 == Mcal_CanFilterCfgTable[chNum].FilterConfig.FilterFIFOAssignment)
     {
-        if (CanNum == Mcal_CanFilterCfgTable[chNum].CanHandle && CAN_RX_FIFO1 == Mcal_CanFilterCfgTable[chNum].FilterConfig.FilterFIFOAssignment)
+      HAL_CAN_GetRxMessage(Mcal_CanFilterCfgTable[chNum].CanHandle, Mcal_CanFilterCfgTable[chNum].FilterConfig.FilterFIFOAssignment, &Mcal_CanCtrl.RxHeader, Mcal_CanCtrl.rcvData);
+      /* Check if the received message is from CAN1_RX_CCP */
+      if (Mcal_CanCtrl.RxHeader.DLC > 0 && Mcal_CanCtrl.RxHeader.StdId == MCAL_CAN1_CCP_RX_ID)
+      {
+        Ccp_CanDrv_ReceiveCallback(Mcal_CanCtrl.RxHeader.StdId, Mcal_CanCtrl.rcvData, Mcal_CanCtrl.RxHeader.DLC);
+      }
+      /* Check if the received message is from CAN2_MCU_STATUS3 */
+      else if (Mcal_CanCtrl.RxHeader.DLC > 0 && Mcal_CanCtrl.RxHeader.ExtId == MCAL_RX_MCU_STATUS3_ID)
+      {
+        if (8 == Mcal_CanCtrl.RxHeader.DLC)
         {
-            HAL_CAN_GetRxMessage(Mcal_CanFilterCfgTable[chNum].CanHandle, Mcal_CanFilterCfgTable[chNum].FilterConfig.FilterFIFOAssignment, &Mcal_CanCtrl.RxHeader, Mcal_CanCtrl.rcvData);
-            if (Mcal_CanCtrl.RxHeader.DLC > 0 && Mcal_CanCtrl.RxHeader.StdId == MCAL_CAN1_CCP_RX_ID)
-            {
-                Ccp_CanDrv_ReceiveCallback(Mcal_CanCtrl.RxHeader.StdId, Mcal_CanCtrl.rcvData, Mcal_CanCtrl.RxHeader.DLC);
-            }
+          MCAL_CYCBUF_WRITE(Mcal_CanCtrl.Buf[MCAL_CAN2_RX_MCU_STATUS3].RcvCycBufID, Mcal_CanCtrl.rcvData, Mcal_CanCtrl.RxHeader.DLC);
         }
+        else
+        {
+         MCAL_ERROR("%s ID:%x RcvErr!\n\r", __FUNCTION__, MCAL_RX_MCU_STATUS3_ID);
+        }
+      }
+      else
+      {
+        // Handle other messages if necessary
+      }
     }
+  }
 }
 
 void Mcal_Can_Init(void)
--- JDL _AC_Ultra-Fast_Charger/Source/BSW/MCAL/Mcal_Can/Mcal_Can_Cfg.h
+++ JDL _AC_Ultra-Fast_Charger/Source/BSW/MCAL/Mcal_Can/Mcal_Can_Cfg.h
@@ -4,33 +4,42 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/* ================== Includes ================== */
+/*******************************************************************************
+|    Other Header File Inclusion
+|******************************************************************************/
 #include "stm32f1xx_hal.h"  // Include the HAL library for STM32F1xx
 #include "Mcal_Types.h"
 
-/* ================== Macros ================== */
-#define MCAL_CAN1_RX_CYC_LEN 256
-
-/* CAN test cfg */
-#define MCAL_CAN1_RX_ID 0x1800D8D0
-#define MCAL_CAN1_TX_ID 0x1800D8D9
-
-#define MCAL_CAN1_FILTER_ID_HIGH (((uint32_t)MCAL_CAN1_RX_ID << 3) & 0xFFFF0000) >> 16                  // High-level ID
-#define MCAL_CAN1_FILTER_ID_LOW (((uint32_t)MCAL_CAN1_RX_ID << 3) | CAN_ID_EXT | CAN_RTR_DATA) & 0xFFFF // Low-level ID
-#define MCAL_CAN1_FILTER_MASK_ID_HIGH (0xffff)                                                          // Filter mask high-level ID
-#define MCAL_CAN1_FILTER_MASK_ID_LOW ((0xfff0 << 3) | CAN_ID_EXT | CAN_RTR_DATA)                        // Filter mask low-level ID
-
-/* CAN CCP cfg */
+/*******************************************************************************
+|    Macro Definition
+|******************************************************************************/
+#define MCAL_CAN_RX_CYC_LEN 256 /* CAN receive circular buffer length */
+/* TEST_ID CFG */
+#define MCAL_CAN1_RX_TEST_ID 0x1800D8D0
+#define MCAL_CAN1_TEST_FILTER_ID_HIGH (((uint32_t)MCAL_CAN1_RX_TEST_ID << 3) & 0xFFFF0000) >> 16                            // High-level ID
+#define MCAL_CAN1_TEST_FILTER_ID_LOW (((uint32_t)MCAL_CAN1_RX_TEST_ID << 3) | CAN_ID_EXT | CAN_RTR_DATA) & 0xFFFF           // Low-level ID
+#define MCAL_CAN1_TEST_FILTER_MASK_ID_HIGH (0xffff)                                                                         // Filter mask high-level ID
+#define MCAL_CAN1_TEST_FILTER_MASK_ID_LOW ((0xfff0 << 3) | CAN_ID_EXT | CAN_RTR_DATA)                                       // Filter mask low-level ID
+/* MCU_STATUS3_ID CFG */
+#define MCAL_RX_MCU_STATUS3_ID 0x18FFA2EF
+#define MCAL_CAN_MCU_STATUS3_FILTER_ID_HIGH (((uint32_t)MCAL_RX_MCU_STATUS3_ID << 3) & 0xFFFF0000) >> 16                    // High-level ID
+#define MCAL_CAN_MCU_STATUS3_FILTER_ID_LOW (((uint32_t)MCAL_RX_MCU_STATUS3_ID << 3) | CAN_ID_EXT | CAN_RTR_DATA) & 0xFFFF   // Low-level ID
+#define MCAL_CAN_MCU_STATUS3_FILTER_MASK_ID_HIGH (0xffff)                                                                   // Filter mask high-level ID
+#define MCAL_CAN_MCU_STATUS3_FILTER_MASK_ID_LOW (0xffff)                                                                    // Filter mask low-level ID
+/* CCP_ID CFG */
 #define MCAL_CAN1_CCP_RX_ID 0x2E1
-#define MCAL_CAN1_CCP_TX_ID 0x2E2
-
-#define MCAL_CAN1_CCP_FILTER_ID_HIGH ((uint16_t)MCAL_CAN1_CCP_RX_ID << 5)                               // High-level ID
-#define MCAL_CAN1_CCP_FILTER_ID_LOW ((uint16_t)MCAL_CAN1_CCP_TX_ID << 5)                                // Low-level ID
-#define MCAL_CAN1_CCP_FILTER_MASK_ID_HIGH (0xffff)                                                 // Filter mask high-level ID
-#define MCAL_CAN1_CCP_FILTER_MASK_ID_LOW (0xffff)                                                  // Filter mask low-level ID
-
-/* ================== Enum ================== */
+#define MCAL_CAN1_CCP_FILTER_ID_HIGH ((uint16_t)MCAL_CAN1_CCP_RX_ID << 5)                                                   // High-level ID
+#define MCAL_CAN1_CCP_FILTER_ID_LOW ((uint16_t)MCAL_CAN1_CCP_TX_ID << 5)                                                    // Low-level ID
+#define MCAL_CAN1_CCP_FILTER_MASK_ID_HIGH (0xffff)                                                                          // Filter mask high-level ID
+#define MCAL_CAN1_CCP_FILTER_MASK_ID_LOW (0xffff)                                                                           // Filter mask low-level ID
+/* CAN TX ID CFG */
+#define MCAL_CAN1_TX_TEST_ID 0x1800D8D9                                                                                     // CAN1 test ID
+#define MCAL_CAN1_CCP_TX_ID 0x2E2                                                                                           // CAN1 CCP TX ID
+#define MCAL_CAN_SECC_MSG1_TX_ID 0x18FF0056                                                                                 // CAN SECC_MSG1 TX ID
+#define MCAL_CAN_SECC_MSG2_TX_ID 0x18FF0156                                                                                 // CAN SECC_MSG2 TX ID
+/*******************************************************************************
+|    Enum Definition
+|******************************************************************************/
 typedef enum
 {
     MCAL_CAN1_CH,      /* CAN1 */
@@ -40,19 +49,26 @@ typedef enum
 
 typedef enum
 {
-    MCAL_CAN1_RX_TEST,       /* CAN1 test */
-    MCAL_CAN1_RX_CCP,     /* CCP RX CH */
-    MCAL_CAN_RX_MAX_NUMBER /* CAN NUM */
+    MCAL_CAN1_RX_TEST,              /* CAN1 test */
+    MCAL_CAN_RX_CCP,                /* CCP RX CH */
+    MCAL_CAN1_RX_MCU_STATUS3,       /* CAN1 MCU STATUS3 RX CH */
+    MCAL_CAN2_RX_MCU_STATUS3,       /* CAN2 MCU STATUS3 RX CH */
+    MCAL_CAN_RX_MAX_NUMBER          /* CAN NUM */
 } Mcal_CanRxChannel_Enum_t;
 
 typedef enum
 {
-    MCAL_CAN1_TX_CH,       /* CAN1 test */
-    MCAL_CAN1_TX_CCP,     /* CCP TX CH */
-    MCAL_CAN_TX_MAX_NUMBER /* CAN NUM */
+    MCAL_CAN1_TX_CH,            /* CAN1 test */
+    MCAL_CAN_TX_CCP,            /* CCP TX CH */
+    MCAL_CAN1_TX_SECC_MSG1,     /* CAN1 SECC_MSG1 */
+    MCAL_CAN1_TX_SECC_MSG2,     /* CAN1 SECC_MSG2 */
+    MCAL_CAN2_TX_SECC_MSG1,     /* CAN2 SECC_MSG1 */
+    MCAL_CAN2_TX_SECC_MSG2,     /* CAN2 SECC_MSG2 */
+    MCAL_CAN_TX_MAX_NUMBER      /* CAN NUM */
 } Mcal_CanTxChannel_Enum_t;
-
-/* ================== Type Definitions ================== */
+/*******************************************************************************
+|    Typedef Definition
+|******************************************************************************/
 typedef struct
 {
     Mcal_CanChannel_Enum_t CanChannel;     /* Can Tx channal */
@@ -76,7 +92,7 @@ typedef struct
 typedef struct
 {
     uint8_t RcvCycBufID;                     /* Receive CycBuf ID */
-    uint8_t RcvCycBuf[MCAL_CAN1_RX_CYC_LEN]; /* Receive the circular buffer */
+    uint8_t RcvCycBuf[MCAL_CAN_RX_CYC_LEN]; /* Receive the circular buffer */
     uint32_t RcvCycBufLen;                   /* Receive the length of the circular buffer */
 } Mcal_Can_RcvBuf_Cfg_t;
 
@@ -88,10 +104,17 @@ typedef struct
     CAN_RxHeaderTypeDef RxHeader;                      /* Receive handle */
     Mcal_Can_RcvBuf_Cfg_t Buf[MCAL_CAN_RX_MAX_NUMBER]; /* Rcv cyc buf Configuration */
 } Mcal_CanCtrlCfg_t;
+/*******************************************************************************
+|	 Static local variables Declaration
+|******************************************************************************/
 
-/* ================== Extern Declarations ================== */
+/*******************************************************************************
+|	 Global Variable with extern linkage
+|******************************************************************************/
 
-/* ================== Function Prototypes ================== */
+/*******************************************************************************
+|	 Global Function Prototypes
+|******************************************************************************/
 void Mcal_Can_Init(void);
 void Mcal_Can_Enable(void);
 void Mcal_Can_Disable(void);
